eee1d3d158e235f6bb4e0718904f6edf3d750be1
Danielmangesto <88393553+Danielmangesto@users.noreply.github.com>
2023-09-17 18:13:39 +0300

    17/09

---
+++ my-app/src/settings.py
@@ -0,0 +1,16 @@
+dbpwd = ''
+db_end_point = ''
+local_db_endpoint= 'localhost'
+aws_access_key_id =''
+aws_secret_access_key = ''
+bucket_name = ''
+
+google_client_id = ""
+google_secret = ""
+
+mail_server = 'smtp.elasticemail.com'
+mail_port = 2525
+mail_password= ''
+mail_username = ''
+
+elastic_api_key = ''
